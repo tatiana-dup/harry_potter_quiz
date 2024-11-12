90f9d2ec2fd7a16be3dbe0848e8f7df4167ab179
Tatiana Duplinskaia <tancher5@gmail.com>
2024-11-12 12:04:09 +0200

    Create apps

---
+++ api_harry_potter_quiz/api_harry_potter_quiz/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for api_harry_potter_quiz project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_harry_potter_quiz.settings')
+
+application = get_asgi_application()
