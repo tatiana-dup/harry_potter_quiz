2d3e48ef761357a05858d4bb80604f1ad6aba960
Tatiana Duplinskaia <tancher5@gmail.com>
2024-11-26 10:39:22 +0200

    Update models for quizzes app

--- api_harry_potter_quiz/quizzes/models.py
+++ api_harry_potter_quiz/quizzes/models.py
@@ -5,11 +5,19 @@ from quizzes.constants import (SLUG_MAX_LENGTH,
 
 
 class BaseNameSlugModel(models.Model):
+    """Абстрактная модель, содержащая название и слаг."""
     name = models.CharField('Название', unique=True)
     slug = models.SlugField('Слаг', max_length=SLUG_MAX_LENGTH, unique=True)
 
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name[:TEXT_LENGTH]
+
 
 class Part(BaseNameSlugModel):
+    """Части истории, к которым могут относиться вопросы. """
     serial_number = models.PositiveSmallIntegerField('Номер части',
                                                      unique=True)
 
@@ -18,22 +26,17 @@ class Part(BaseNameSlugModel):
         verbose_name_plural = 'Части'
         ordering = ('serial_number',)
 
-    def __str__(self):
-        return self.name[:TEXT_LENGTH]
-
 
 class Tag(BaseNameSlugModel):
-
+    """Тэги для вопросов."""
     class Meta:
         verbose_name = 'тэг'
         verbose_name_plural = 'Тэги'
         ordering = ('name',)
 
-    def __str__(self):
-        return self.name[:TEXT_LENGTH]
-
 
 class Question(models.Model):
+    """Вопросы для квизов."""
     DIFFICULTY_CHOICES = [
         (1, 'Легкий'),
         (2, 'Средний'),
@@ -43,11 +46,20 @@ class Question(models.Model):
 
     text = models.TextField('Текст вопроса')
     answer_requirements = models.TextField(
-        'Пояснение, каким должен быть ответ', null=True, blank=True)
-    image = models.ImageField('Изображение',
-                              upload_to='quizzes/questions/images')
-    difficulty_level = models.IntegerField(choices=DIFFICULTY_CHOICES,
-                                           default=1)
+        'Требования к ответу',
+        help_text='Пояснения или подсказка, каким должен быть ответ',
+        null=True,
+        blank=True)
+    image = models.ImageField(
+        'Изображение',
+        help_text='Добавьте изображение, если оно нужно для вопроса.',
+        upload_to='quizzes/questions/images',
+        null=True,
+        blank=True)
+    difficulty_level = models.IntegerField(
+        'Сложность',
+        choices=DIFFICULTY_CHOICES,
+        default=1)
     part = models.ForeignKey(
         Part,
         null=True,
@@ -60,11 +72,14 @@ class Question(models.Model):
         verbose_name='Тэги',
         related_name='questions')
     is_answer_in_book = models.BooleanField(
-        'В книге есть ответ на этот вопрос?')
+        'По книгам',
+        help_text='В книгах есть ответ на этот вопрос?')
     is_answer_in_movie = models.BooleanField(
-        'В фильме есть ответ на этот вопрос?')
+        'По фильмам',
+        help_text='В фильмах есть ответ на этот вопрос?')
     is_active = models.BooleanField(
-        'Доступен пользователю?', default=False)
+        'Доступность',
+        help_text='Вопрос доступен пользователю?', default=False)
 
     class Meta:
         verbose_name = 'вопрос'
@@ -76,9 +91,18 @@ class Question(models.Model):
 
 
 class Answer(models.Model):
+    """Варианты ответов на вопросы."""
     text = models.TextField('Текст ответа')
-    description = models.TextField('Пояснение', null=True, blank=True)
+    question = models.ForeignKey(
+        Question,
+        on_delete=models.CASCADE,
+        verbose_name='Вопрос',
+        related_name='answers')
     is_correct = models.BooleanField('Это верный ответ?')
+    description = models.TextField(
+        'Пояснение',
+        help_text='Можно указать, почему этот ответ является верным или нет.',
+        null=True, blank=True)
 
     class Meta:
         verbose_name = 'ответ'
@@ -86,5 +110,11 @@ class Answer(models.Model):
         ordering = ('text',)
 
 
-class QuestionCollection(models.Model):
-    pass
+class QuestionCollection(BaseNameSlugModel):
+    """Коллекции вопросов по общей тематике."""
+    description = models.TextField('Описание')
+    questions = models.ManyToManyField(
+        Question,
+        verbose_name='Вопросы',
+        related_name='collections'
+    )
